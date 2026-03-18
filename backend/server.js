5be26b454cc2cba55772215fbf8687ff7834da4a
bigchunguss42069 <silvan_schrag@hotmail.com>
2026-03-18 14:41:30 +0100

    merge 2

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ const PORT = Number(process.env.PORT) || 3000;
 app.use(cors());
 app.use(express.json({ limit: '25mb' }));
 
-app.use('/src', express.static(path.join(__dirname, 'src')));
-app.use(express.static(__dirname));
 
 
 // ============================================================================
@@ -3787,9 +3785,6 @@ app.get(
 );
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 
 // ============================================================================
